62e44344899d9e528b23f1161974760229d86ee9
Jexus <b05901033@ntu.edu.tw>
2017-11-22 16:12:51 +0800

    on ubuntu

--- flaskapp.py
+++ flaskapp.py
@@ -1,28 +1,33 @@
 # -*- coding: utf8 -*-
-from flask import Flask
+from flask import Flask, jsonify
+from flask import make_response
+from flask import request
+from flask import abort
 import os, csv, json
+import codecs
 app = Flask(__name__)
 
-@app.route('/index.html')
+@app.route('/')
 def index():
     text = '''
         <!DOCTYPE HTML>
         <title>User Manual</title>
         <h1 align="center">-&emsp;-&emsp;-&emsp;&emsp;使用說明&emsp;&emsp;-&emsp;-&emsp;- </h1>
-        <pre>(1) http://114.34.123.174:8080/index.html       ---> 查看使用說明</pre>
-        <pre>(2) http://114.34.123.174:8080/                 ---> 生成path<->stop資料(不會用到，由其他API使用)</pre>
-        <pre>(3) http://114.34.123.174:8080/                 ---> 生成Map Net資料(不會用到，由其他API使用)</pre>
-        <pre>(4) http://114.34.123.174:8080/{緯度}/{經度}     ---> 查詢附近站牌</pre>
+    <pre>(1) <a href="http://114.34.123.174:8080/">http://114.34.123.174:8080/</a>       ---> 查看使用說明</pre>
+        <pre>(2) <a href="http://114.34.123.174:8080/stolen">http://114.34.123.174:8080/stolen</a>           ---> 新竹犯罪資料</pre>
+        <pre>(3) <a href="http://114.34.123.174:8080/fireDep">http://114.34.123.174:8080/fireDep</a>           --->消防局資料 </pre>
+        <pre>(4) http://114.34.123.174:8080/{緯度}/{經度}    ---> </pre>
         '''
     return text
 
 @app.route('/stolen')
 def stolen():
+    os.chdir("/home/yung-sung/HsinChu/ill")
     final = []
     for root, dirs, files in os.walk("."):
         for f in files:
             filename = str(f)
-            print(filename,"\n------------\n")
+            #print(filename,"\n------------\n")
             if filename.startswith('1'):
                 csvf = open(filename,encoding = 'utf8')
                 csvr = csv.reader(csvf)
@@ -31,10 +36,16 @@ def stolen():
                         row[0] = row[0].rstrip()
                         row = row[0:3]
                         final.append(row)
-                        print(row)
+                        #print(row)
     result = {"data":final}
     return jsonify(result)
 
+@app.route('/fireDep')
+def fireDep():
+    os.chdir("/home/yung-sung/HsinChu/emerg")
+    f = codecs.open("fireDep.json", 'r', 'utf-8-sig')
+    j = json.load(f)
+    return jsonify(j)
 
 
 if __name__ == '__main__':
