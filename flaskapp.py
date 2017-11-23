3b26045ca0853c064914f5ba6496792896e0d5e8
Jexus <b05901033@ntu.edu.tw>
2017-11-23 12:52:36 +0800

    3

--- flaskapp.py
+++ flaskapp.py
@@ -83,6 +83,35 @@ def fireDep():
     j = json.load(f)
     return jsonify(j)
 
+@app.route('/emerg')
+def emerg():
+    os.chdir("/home/yung-sung/HsinChu/emerg")
+    file = codecs.open('AED.json', 'r', 'utf-8-sig')
+    a = json.loads(file.read())
+
+    file = codecs.open('emergPlace.json', 'r', 'utf-8-sig')
+    e = json.loads(file.read())
+
+    file = codecs.open('fireDep.json', 'r', 'utf-8-sig')
+    f = json.loads(file.read())
+
+    file = codecs.open('hydrant.json', 'r', 'utf-8-sig')
+    h = json.loads(file.read())
+
+    file = codecs.open('monitor.json', 'r', 'utf-8-sig')
+    m = json.loads(file.read())
+
+    file = codecs.open('police.json', 'r', 'utf-8-sig')
+    p = json.loads(file.read())
+
+    result = {"AED":a,
+            "emergPlace":e,
+            "fireDep":f,
+            "hydrant":h,
+            "monitor":m,
+            "police":p}
+    return jsonify(result)
+
 
 if __name__ == '__main__':
     app.run()
