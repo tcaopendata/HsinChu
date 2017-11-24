3fade9da31d05f53f1c91f196ed53d07b3b6a4b4
Jexus <zetanollyjs-cp@yahoo.com.tw>
2017-11-25 00:31:02 +0800

    10

--- emerg/hydrant.py
+++ emerg/hydrant.py
@@ -1,26 +1,31 @@
+# -*- coding:utf8 -*-
 import os, json, codecs
 import googlemaps
+import time
+import random
 
 def dig_address(words,lg):
     if lg == 0:
         print("totally fails!------------------\n\n")
-        return (24.8049301,120.9723226)
-    geocode_result = gmaps.geocode(address=words[lg], language='zh-TW')
+        return (24.8049301+random.uniform(-0.05,0.05),120.9723226+random.uniform(-0.05,0.05))
+    geocode_result = gmaps.geocode(address=words[:lg], language='zh-TW')
     if len(geocode_result) != 0:
-        lat = geocode_result[0]['geometry']['location']['lat']
-        lng = geocode_result[0]['geometry']['location']['lng']
-        if pow(lat - 24,2) > 4 or pow(lng - 120,2) >4:
-            print("geo fails!")
-            print(lat, lng)
-            print(words[:lg])
-            print("dig in len =", lg - 1)
-            return dig_address(words, lg - 1)
-        else:
-            print("dig_succeed! in len =",lg,"---------------------")
-            print(words[:lg])
-            print(lat, lng)
-            print('\n\n')
-            return (lat, lng)
+        idx = 0
+        while(idx<len(geocode_result)):
+            lat = geocode_result[idx]['geometry']['location']['lat']
+            lng = geocode_result[idx]['geometry']['location']['lng']
+            if(pow(lat - 24, 2) < 4 and pow(lng - 120, 2) < 4):
+                print("dig_succeed! in len =", lg, "---------------------",words[:lg],lat, lng)
+                if(lg<=3):
+                    return (24.8049301+random.uniform(-0.05,0.05),120.9723226+random.uniform(-0.05,0.05))
+                else:
+                    return (lat, lng)
+            idx+=1
+        print("geo fails!")
+        print(lat, lng)
+        print(words[:lg])
+        print("dig in len =", lg - 1)
+        return dig_address(words, lg - 1)
     else:
         print("still fails!")
         print(words[:lg])
@@ -28,13 +33,15 @@ def dig_address(words,lg):
         return dig_address(words,lg-1)
 
 
-gmaps = googlemaps.Client(key='AIzaSyDr3z6Nu2lBSE1bZqg-u-oeENCaF53Xhcs')
+gmaps = googlemaps.Client(key='AIzaSyADIUvx0j9HZqiSDEVpzR7DiN4zynB3zZw')
 #geocode_result = gmaps.geocode(address=words, language='zh-TW')
 
 file = codecs.open('hydrant.json', 'r', 'utf-8-sig')
 data = json.loads(file.read())
 
 for i in data:
+    if "lat" in i.keys():
+        continue
     geocode_result = gmaps.geocode(address=i["設置地點"], language='zh-TW')
     if len(geocode_result) != 0:
         lat = geocode_result[0]['geometry']['location']['lat']
@@ -43,11 +50,13 @@ for i in data:
         i["lat"]=str(lat)
         i["lng"]=str(lng)
     else:
-        print("fail!---------start digging")
-        print(i["設置地點"])
+        print("fail!---------start digging",i["設置地點"],"---------------------------------")
         result = dig_address(i["設置地點"],len(i["設置地點"])-2)
         i["lat"]=str(result[0])
         i["lng"]=str(result[1])
+    with open('hydrant.json', 'w') as file1:
+        json.dump(data, file1)
+    file1.close()
 
 with open('hydrant.json', 'w') as file1:
     json.dump(data, file1)
