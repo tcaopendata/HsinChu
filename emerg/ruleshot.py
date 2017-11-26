86a5db0525ed0ef07ad63bd88a4af7c2518e5d5c
Jexus <zetanollyjs-cp@yahoo.com.tw>
2017-11-26 09:13:02 +0800

    32

--- emerg/ruleshot.py
+++ emerg/ruleshot.py
@@ -21,44 +21,44 @@ rec = {}
 for i in data:
     if "lat" in i.keys():
         continue
-    print(i["攝影機地點 "])
+    print(i["違規地點"])
 
     sta = -1
-    while (sta < len(i["攝影機地點 "])):
+    while (sta < len(i["違規地點"])):
         sta += 1
-        if is_chinese(i["攝影機地點 "][sta]):
+        if is_chinese(i["違規地點"][sta]):
             break
 
     pos = 0
-    while (pos < len(i["攝影機地點 "])):
+    while (pos < len(i["違規地點"])):
         pos += 1
-        if i["攝影機地點 "][pos] in ["、", "與", "上", "(", "口", "匝", "往", "制", "前"]:
+        if i["違規地點"][pos] in ["、", "與", "上", "(", "口", "匝", "往", "制", "前"]:
             break
-        if i["攝影機地點 "][pos] in ["巷", "段", "號", "街", "弄", "場"]:
+        if i["違規地點"][pos] in ["巷", "段", "號", "街", "弄", "場"]:
             pos += 1
             break
 
-    if i["攝影機地點 "][sta:pos] in rec.keys():
-        lat, lng=rec[i["攝影機地點 "][sta:pos]]
+    if i["違規地點"][sta:pos] in rec.keys():
+        lat, lng=rec[i["違規地點"][sta:pos]]
         i["lat"] = str(lat)
         i["lng"] = str(lng)
         with open('ruleshot.json', 'w') as file1:
             json.dump(data, file1)
         file1.close()
-        print("直接套用",i["攝影機地點 "][sta:pos])
+        print("直接套用",i["違規地點"][sta:pos])
         continue
 
 
-    geocode_result = gmaps.geocode(address="新竹"+i["攝影機地點 "][sta:pos], language='zh-TW')
+    geocode_result = gmaps.geocode(address="新竹"+i["違規地點"][sta:pos], language='zh-TW')
     if len(geocode_result) != 0:
         lat = geocode_result[0]['geometry']['location']['lat']
         lng = geocode_result[0]['geometry']['location']['lng']
-        print("找到了",i["攝影機地點 "], " -> ",i["攝影機地點 "][sta:pos],lat, lng)
-        rec[i["攝影機地點 "][sta:pos]]=(lat, lng)
+        print("找到了",i["違規地點"], " -> ",i["違規地點"][sta:pos],lat, lng)
+        rec[i["違規地點"][sta:pos]]=(lat, lng)
         i["lat"]=str(lat)
         i["lng"]=str(lng)
     else:
-        print("\n失敗",i["攝影機地點 "], " -> ",i["攝影機地點 "][sta:pos],"---------------------------------")
+        print("\n失敗",i["違規地點"], " -> ",i["違規地點"][sta:pos],"---------------------------------")
         result = (24.8049301 + random.uniform(-0.05, 0.05), 120.9723226 + random.uniform(-0.05, 0.05))
         print("亂數分配",result)
         i["lat"] = str(result[0])
